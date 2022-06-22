c0f1db426e23cb0bb4464e77c357e2523596f5f1
Bhanu-sai-achari <bhanu.rymec@gmail.com>
2022-06-22 12:55:23 +0530

    #error fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-const DATABASE ="mongodb://localhost:27017/testup";
+const DATABASE ="mongodb://mymongo:27017/testup";
 
 mongoose.connect(DATABASE)
 .then(()=>{
