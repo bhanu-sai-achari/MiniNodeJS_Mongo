6f0e0a21a9ec6c574db38d551ae1421519c9fc29
Bhanu-sai-achari <bhanu.rymec@gmail.com>
2022-06-22 12:19:19 +0530

    Docker-compose file added

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+
+const DATABASE ="mongodb://localhost:27017/testup";
+
+mongoose.connect(DATABASE)
+.then(()=>{
+    console.log("DB Connected!!!!!!!!")
+})
+.catch(()=>{
+    console.log("DB not Connected!!!!!!!!")
+
+})
